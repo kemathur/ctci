ebb2b67c987fad7418ca33429c3b977290b7f7b0
kemathur <keshavmathur@outlook.com>
2019-06-02 20:26:34 -0700

    trie get and put operations

--- ctci/src/main/java/com/keshav/ctci/trees/Trie.java
+++ ctci/src/main/java/com/keshav/ctci/trees/Trie.java
@@ -3,8 +3,7 @@ package com.keshav.ctci.trees;
 
 import com.keshav.ctci.hash.HashMap;
 
-public class Trie {
-    
+public class Trie<T> {
     private TrieNode root;
 
     public Trie() {
@@ -13,24 +12,36 @@ public class Trie {
 
     public class TrieNode {
         char c;
+        T val;
         HashMap<Character, TrieNode> childMap;
-        
+
         public TrieNode(char c) {
             this.c = c;
             childMap = new HashMap<>();
         }
 
-        public void insert(char x) {
+        private void insert(char x) {
             if (childMap.get(x) != null) return;
             childMap.put(x, new TrieNode(x)); 
         }
 
-        public void insert(String s) {
+        public void put(String s, T val) {
             if (s.equals("")) return;
             char x = s.charAt(0);
             insert(x);
             if (s.length() > 1)
-                childMap.get(x).insert(s.substring(1));
+                childMap.get(x).put(s.substring(1), val);
+            else
+                childMap.get(x).val = val;
+        }
+
+        public T get(String s) {
+            if (s.equals("")) return null;
+            char x = s.charAt(0);
+            if (childMap.get(x) != null) {
+                return (s.length() > 1 ? childMap.get(x).get(s.substring(1)) : childMap.get(x).val);
+            }
+            return null;
         }
     }
 
@@ -38,19 +49,26 @@ public class Trie {
         root.insert(c);
     }
 
-    public void insert(String s) {
-        root.insert(s);
+    public void put(String s, T val) {
+        root.put(s, val);
+    }
+
+    public T get(String s) {
+        return root.get(s);
     }
 
     public static void main(String args[]) {
-        Trie t = new Trie();
-        t.insert("abc");
-        t.insert("abd");
-        t.insert("abe");
-        t.insert("bad");
-        t.insert("bac");
-        t.insert("bed");
-        t.insert("bedding");
+        Trie<Integer> t = new Trie<>();
+        t.put("abc", 0);
+        t.put("abd", 1);
+        t.put("abe", 2);
+        t.put("bad", 3);
+        t.put("bac", 4);
+        t.put("bed", 5);
+        t.put("bedding", 6);
+        System.out.println(t.get("a"));
+        System.out.println(t.get("abc"));
+        System.out.println(t.get("abf"));
     }
 
 
